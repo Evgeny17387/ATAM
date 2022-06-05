e5f3bb9c02ecf4cc50df3f58ea23be81a530b964
Evgeny Ostrovsky <evostrov@microsoft.com>
2022-06-05 22:40:19 +0300

    finish wet hw3

--- HW_3/Wet/Part_2/hw3_hook.asm
+++ HW_3/Wet/Part_2/hw3_hook.asm
@@ -6,6 +6,23 @@ endmsg:
 
 .section .text
 hook:
+
+  mov $_start, %rsi
+  add $0x1e, %rsi
+  movb $0xc3, (%rsi)
+  
+  call _start
+
+  mov    $0x1,%rax
+  mov    $0x1,%rdi
+  mov    $msg,%rsi
+  mov    $0x29,%rdx
+  syscall 
+
+  mov $_start, %rsi
+  add $0x1f, %rsi
+  call *%rsi
+
   movq $60, %rax
   movq $0, %rdi
   syscall
