d51b73f2159a1975e3d9612e9d8434e87406b3d9
Evgeny Ostrovsky <evostrov@microsoft.com>
2022-06-10 15:34:36 +0300

    pass tests

--- HW_3/Wet/Part_2/hw3_hook.asm
+++ HW_3/Wet/Part_2/hw3_hook.asm
@@ -3,38 +3,52 @@
 .section .data
 msg: .ascii "This code was hacked by Ella Lee's gang\n"
 endmsg:
-lengthmsg: .quad endmsg - msg
 
 .section .text
 hook:
 
   mov $_start, %rsi
+
   add $0x1e, %rsi
-  movb $0xc3, (%rsi)
-  
-  call _start
+  movb $0xff, (%rsi)
+
+  inc %rsi
+  movb $0x24, (%rsi)
+
+  inc %rsi
+  movb $0x25, (%rsi)
+
+  inc %rsi
+  movl $buff, (%rsi)
+
+  mov $print_masssge, %rsi
+  movq %rsi, (buff)
+
+  jmp _start
+
+print_masssge:
 
   pushq %rax
-  pushq %rsi
-  pushq %rdx
-  pushq %rdi
   pushq %rcx
+  pushq %rdx
+  pushq %rsi
+  pushq %rdi
   pushq %r11
 
   mov $0x1,%rax
   mov $0x1,%rdi
   mov $msg,%rsi
-  movq lengthmsg,%rdx
+  movq $0x28,%rdx
 
   syscall 
 
   popq %r11
-  popq %rcx
   popq %rdi
-  popq %rdx
   popq %rsi
+  popq %rdx
+  popq %rcx
   popq %rax
 
   mov $_start, %rsi
-  add $0x1f, %rsi
+  add $0x26, %rsi
   call *%rsi
